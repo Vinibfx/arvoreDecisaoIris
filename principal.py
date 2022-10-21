77c880dbbc529d131b67b99f2be2291e878a18f4
Vinibfx <116393152+Vinibfx@users.noreply.github.com>
2022-10-21 19:45:56 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -1,4 +1,25 @@
 import streamlit as st
+from sklearn.ensemble import RandomForestClassifier 
+import pandas as pd
+dados = pd.read_csv('/content/drive/MyDrive/2022/IA-Engenharia/florestaRandomica/Iris.csv')
+
+classes = dados['Species']
+nomesColunas = dados.columns.to_list()
+tamanho = len(nomesColunas)
+nomesColunas = nomesColunas[1:tamanho-1]
+features = dados[nomesColunas]
+
+from sklearn.model_selection import train_test_split
+
+features_treino,features_teste,classes_treino,classes_teste = train_test_split(features,
+                                                                               classes,
+                                                                               test_size=0.26,
+                                                                               random_state=3)
+
+floresta = RandomForestClassifier(n_estimators=90) 
+
+floresta.fit(features_treino,classes_treino)
+predicoes = floresta.predict(features_teste)
 
 st.title('Aplicativo de IA')
 nome = st.text_input('Digite o seu nome:')
